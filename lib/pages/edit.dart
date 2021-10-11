246627b49d7705c1ecea6d26cd0be79b897c3bc4
Egemen <egemenongel@hotmail.com>
2021-10-11 16:02:38 +0300

    Update Edit Form

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -14,6 +14,7 @@ class EditPage extends StatelessWidget {
         Provider.of<ExpenseListModel>(context, listen: false);
     var _index = _expenseListStore.listIndex;
     var _expenseList = _expenseListStore.expenseList;
+    final _formKey = GlobalKey<FormState>();
     TextEditingController descriptionController = TextEditingController();
     TextEditingController costController = TextEditingController();
     TextEditingController categoryController = TextEditingController();
@@ -22,59 +23,82 @@ class EditPage extends StatelessWidget {
     costController.text = _expenseList[_index].cost.toString();
     categoryController.text = _expenseList[_index].category.toString();
     dateController.text = _expenseList[_index].date.toString();
+
+    String? _costValidation(String? value) {
+      if (value!.isEmpty) {
+        return "This field cannot be empty";
+      } else if (double.tryParse(value) == null) {
+        return "Please type a number";
+      }
+    }
+
+    String? _validation(String? value) {
+      if (value!.isEmpty) {
+        return "This field cannot be empty";
+      }
+    }
+
+    void _editForm() {
+      ExpenseModel expenseModel = ExpenseModel(
+        description: descriptionController.text,
+        cost: int.parse(costController.text),
+        date: DateTime.parse(dateController.text),
+        category: categoryController.text,
+      );
+      _expenseListStore.editExpense(_index, expenseModel);
+    }
+
     return Scaffold(
         appBar: AppBar(
             title: const Text(
           "Edit Expense",
         )),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                controller: descriptionController,
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    controller: costController,
+                    validator: (value) => _costValidation(value),
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    validator: (value) => _validation(value),
+                  ),
+                  TextFormField(
+                    controller: categoryController,
+                    validator: (value) => _validation(value),
+                  ),
+                  DateTimePicker(
+                    type: DateTimePickerType.date,
+                    dateMask: 'd MMM, yyyy',
+                    initialValue: dateController.text,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2100),
+                    icon: const Icon(Icons.event),
+                    dateLabelText: 'Date',
+                    timeLabelText: "Hour",
+                    onChanged: (val) => dateController.text = val,
+                    validator: (value) => _validation(value),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  ElevatedButton(
+                    child: const Text("Edit"),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _editForm();
+                        Navigator.pop(context);
+                      }
+                    },
+                  )
+                ],
               ),
-              TextFormField(
-                controller: costController,
-              ),
-              TextFormField(
-                controller: categoryController,
-              ),
-              DateTimePicker(
-                type: DateTimePickerType.date,
-                dateMask: 'd MMM, yyyy',
-                initialValue: dateController.text,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),
-                icon: const Icon(Icons.event),
-                dateLabelText: 'Date',
-                timeLabelText: "Hour",
-                onChanged: (val) => dateController.text = val,
-                validator: (val) {
-                  return null;
-                },
-                onSaved: (val) => dateController.text = val!,
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              ElevatedButton(
-                child: const Text("Edit"),
-                onPressed: () {
-                  ExpenseModel expenseModel = ExpenseModel(
-                    description: descriptionController.text,
-                    cost: int.parse(costController.text),
-                    date: DateTime.parse(dateController.text),
-                    category: categoryController.text,
-                  );
-                  _expenseListStore.editExpense(_index, expenseModel);
-                  Navigator.pop(context);
-                },
-              )
-            ],
-          ),
-        ));
+            )));
   }
 }
