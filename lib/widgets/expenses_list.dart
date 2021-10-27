14128295d3534cbe534189b18c276bc283b9b672
Egemen <egemenongel@hotmail.com>
2021-10-27 20:29:24 +0300

    Create Expenses List  widget

---
+++ lib/widgets/expenses_list.dart
@@ -0,0 +1,26 @@
+import 'package:biobuluyo_app/models/expense_list.dart';
+import 'package:biobuluyo_app/widgets/expense_tile.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var _expenseListModel =
+        Provider.of<ExpenseListModel>(context, listen: true);
+    return ListView.separated(
+      itemCount: _expenseListModel.expenseList.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(
+        color: Colors.transparent,
+        height: 1,
+      ),
+      itemBuilder: (BuildContext context, int index) {
+        return ExpenseTile(
+          index: index,
+        );
+      },
+    );
+  }
+}
