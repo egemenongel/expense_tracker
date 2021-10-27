cb3979b01e7d191d5d49fc48c89ffd0985c17f9b
Egemen <egemenongel@hotmail.com>
2021-10-27 20:29:01 +0300

    Create Expense Tile widget

---
+++ lib/widgets/expense_tile.dart
@@ -0,0 +1,66 @@
+import 'package:biobuluyo_app/models/expense_list.dart';
+import 'package:biobuluyo_app/pages/edit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:provider/provider.dart';
+
+class ExpenseTile extends StatelessWidget {
+  ExpenseTile({Key? key, required this.index}) : super(key: key);
+  int index;
+  @override
+  Widget build(BuildContext context) {
+    var _expenseListModel =
+        Provider.of<ExpenseListModel>(context, listen: false);
+    var list = _expenseListModel.expenseList;
+    return Slidable(
+      child: ListTile(
+        leading: const Icon(Icons.location_on),
+        title: Row(
+          children: [
+            Text("${list[index].description} "),
+            const Expanded(child: SizedBox()),
+            Text("${list[index].cost} TL"),
+          ],
+        ),
+        subtitle: Row(
+          children: [
+            Text(list[index].date.toString().substring(0, 10)),
+            const Expanded(child: SizedBox()),
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Text(
+                  "${list[index].category}",
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+      actionPane: const SlidableScrollActionPane(),
+      actionExtentRatio: 0.20,
+      actions: [
+        IconSlideAction(
+          caption: "Edit",
+          icon: Icons.edit,
+          color: Colors.blue,
+          onTap: () {
+            _expenseListModel.setIndex(index);
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const EditPage()));
+          },
+        ),
+      ],
+      secondaryActions: [
+        IconSlideAction(
+          caption: "Remove",
+          icon: Icons.remove,
+          color: Colors.red,
+          onTap: () => _expenseListModel.removeExpense(index),
+        ),
+      ],
+    );
+  }
+}
