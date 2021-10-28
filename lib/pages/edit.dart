ead388cc023f8d252807b43c86d9cfd443b493e6
Egemen <egemenongel@hotmail.com>
2021-10-28 15:16:40 +0300

    Refactor code

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -11,29 +11,14 @@ class EditPage extends StatelessWidget {
     var _expenseListStore =
         Provider.of<ExpenseListModel>(context, listen: false);
     var _selectedItem = _expenseListStore.expenseList[index];
-    final _formKey = GlobalKey<FormState>();
     TextEditingController descriptionController = TextEditingController();
     TextEditingController costController = TextEditingController();
     TextEditingController categoryController = TextEditingController();
     TextEditingController dateController = TextEditingController();
     descriptionController.text = _selectedItem.description!;
-    costController.text = _selectedItem.cost.toString();
     categoryController.text = _selectedItem.category!;
+    costController.text = _selectedItem.cost.toString();
     dateController.text = _selectedItem.date.toString();
-
-    String? _costValidation(String? value) {
-      if (value!.isEmpty) {
-        return "This field cannot be empty";
-      } else if (double.tryParse(value) == null) {
-        return "Please type a number";
-      }
-    }
-
-    String? _validation(String? value) {
-      if (value!.isEmpty) {
-        return "This field cannot be empty";
-      }
-    }
 
     return Scaffold(
         appBar: AppBar(
@@ -43,25 +28,22 @@ class EditPage extends StatelessWidget {
         body: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 40),
             child: Form(
-              key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   TextFormField(
                     controller: costController,
-                    validator: (value) => _costValidation(value),
                   ),
                   TextFormField(
                     controller: descriptionController,
-                    validator: (value) => _validation(value),
                   ),
                   TextFormField(
                     controller: categoryController,
-                    validator: (value) => _validation(value),
                   ),
                   DateField(
-                      controller: dateController, validation: _validation),
+                    controller: dateController,
+                  ),
                   const SizedBox(
                     height: 60,
                   ),
