a53ce8f7ebf5c190217c1b51d015d88da56a524a
Egemen <egemenongel@hotmail.com>
2021-10-29 14:57:35 +0300

    Polish Edit Page UI and Refactor code

--- lib/pages/edit.dart
+++ lib/pages/edit.dart
@@ -1,5 +1,4 @@
-import 'package:biobuluyo_app/utils/form_validation.dart';
-import 'package:biobuluyo_app/widgets/date_field.dart';
+import 'package:biobuluyo_app/widgets/expense_form.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:biobuluyo_app/models/expense_list.dart';
@@ -13,62 +12,59 @@ class EditPage extends StatelessWidget {
         Provider.of<ExpenseListModel>(context, listen: false);
     var _selectedItem = _expenseListStore.expenseList[index];
     final _formKey = GlobalKey<FormState>();
-    TextEditingController descriptionController = TextEditingController();
-    TextEditingController costController = TextEditingController();
-    TextEditingController categoryController = TextEditingController();
-    TextEditingController dateController = TextEditingController();
-    descriptionController.text = _selectedItem.description!;
-    costController.text = _selectedItem.cost.toString();
-    categoryController.text = _selectedItem.category!;
-    dateController.text = _selectedItem.date.toString();
+    TextEditingController _descriptionController = TextEditingController();
+    TextEditingController _costController = TextEditingController();
+    TextEditingController _categoryController = TextEditingController();
+    TextEditingController _dateController = TextEditingController();
+    _descriptionController.text = _selectedItem.description!;
+    _costController.text = _selectedItem.cost.toString();
+    _categoryController.text = _selectedItem.category!;
+    _dateController.text = _selectedItem.date.toString();
 
     return Scaffold(
-        appBar: AppBar(
-            title: const Text(
-          "Edit Expense",
-        )),
-        body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextFormField(
-                    controller: costController,
-                    validator: (value) => FormValidation.type(value),
+      appBar: AppBar(
+          title: const Text(
+        "Edit Expense",
+      )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            ExpenseForm(
+              description: _descriptionController,
+              cost: _costController,
+              category: _categoryController,
+              date: _dateController,
+              formKey: _formKey,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: ElevatedButton(
+                      child: const Text("Edit"),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _expenseListStore.editExpense(
+                              index: index,
+                              cost: int.parse(_costController.text),
+                              description: _descriptionController.text,
+                              category: _categoryController.text,
+                              date: DateTime.parse(_dateController.text));
+                          Navigator.pop(context);
+                        }
+                      },
+                    ),
                   ),
-                  TextFormField(
-                    controller: descriptionController,
-                    validator: (value) => FormValidation.empty(value),
-                  ),
-                  TextFormField(
-                    controller: categoryController,
-                    validator: (value) => FormValidation.empty(value),
-                  ),
-                  DateField(
-                      controller: dateController,
-                      validation: FormValidation.empty),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  ElevatedButton(
-                    child: const Text("Edit"),
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        _expenseListStore.editExpense(
-                            index: index,
-                            cost: int.parse(costController.text),
-                            description: descriptionController.text,
-                            category: categoryController.text,
-                            date: DateTime.parse(dateController.text));
-                        Navigator.pop(context);
-                      }
-                    },
-                  )
-                ],
-              ),
-            )));
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
