f9604be77893223849a413639c5a5d98e4888ede
Egemen <egemenongel@hotmail.com>
2021-11-01 14:27:45 +0300

    Change spacing size on top

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -13,59 +13,59 @@ class CategoriesPage extends StatelessWidget {
     var categoryList = _expenseListManager.categoryList;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-            child: Text(
-          "Categories",
-          style: Theme.of(context).textTheme.headline4,
-        )),
-        automaticallyImplyLeading: false,
-      ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 150,
-          ),
-          Expanded(
-              child: ListView.separated(
-            separatorBuilder: (BuildContext context, int index) =>
-                const Divider(
-              color: Colors.transparent,
-              height: 10,
-            ),
-            itemCount: categoryList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50.0),
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blueGrey[300],
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(color: Colors.black, width: 2)),
-                    child: Theme(
-                      child: ListTile(
-                        title: Center(
-                            child: Text(
-                          categoryList[index],
-                          style: const TextStyle(color: Colors.white),
-                        )),
-                        onTap: () {
-                          _expenseListManager
-                              .expenseByCategory(categoryList[index]);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      const ExpenseByCategoryPage()));
-                        },
-                      ),
-                      data: ThemeData(highlightColor: Colors.blueAccent),
-                    )),
-              );
-            },
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            "Categories",
+            style: Theme.of(context).textTheme.headline4,
           )),
-        ],
-      ),
-    );
+          automaticallyImplyLeading: false,
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 100.0,
+            ),
+            Expanded(
+              child: ListView.separated(
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(
+                  color: Colors.transparent,
+                  height: 10,
+                ),
+                itemCount: categoryList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey[300],
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(color: Colors.black, width: 2)),
+                        child: Theme(
+                          child: ListTile(
+                            title: Center(
+                                child: Text(
+                              categoryList[index],
+                              style: const TextStyle(color: Colors.white),
+                            )),
+                            onTap: () {
+                              _expenseListManager
+                                  .expenseByCategory(categoryList[index]);
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const ExpenseByCategoryPage()));
+                            },
+                          ),
+                          data: ThemeData(highlightColor: Colors.blueAccent),
+                        )),
+                  );
+                },
+              ),
+            )
+          ],
+        ));
   }
 }
